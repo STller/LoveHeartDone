0ffe900551e05a5898519827740690e5b445f36c
STller <1165975843@qq.com>
2022-11-23 00:08:47 +0800

    Update index.tsx

--- src/pages/JumpHeart/index.tsx
+++ src/pages/JumpHeart/index.tsx
@@ -8,27 +8,36 @@ import { MeshSurfaceSampler } from "three/examples/jsm/math/MeshSurfaceSampler";
 import { gsap } from "gsap";
 import { Canvas, useLoader } from "@react-three/fiber";
 import styled from "styled-components";
+import { Environment, OrbitControls } from "@react-three/drei";
 
 const JumpHeartContainer = styled.div`
-    width: 100vw;
-    height: 100vh;
-`
+  width: 100vw;
+  height: 100vh;
+`;
 
 function Heart() {
-    const g = useLoader(OBJLoader, 'https://assets.codepen.io/127738/heart_2.obj')
-    const m = g.children[0] as Mesh
-    return <group position={[0,0,0]}>
-        <primitive object={m} />
+  const g = useLoader(
+    OBJLoader,
+    "https://assets.codepen.io/127738/heart_2.obj"
+  );
+  const m = g.children[0] as Mesh;
+  return (
+    <group position={[0, 0, 0]} scale={0.04} rotation={[-Math.PI * 0.5, 0, 0]}>
+      <primitive object={m} />
+      <Environment background preset="sunset" />
     </group>
+  );
 }
 
 function JumpHeart() {
-
-  return <JumpHeartContainer>
-    <Canvas>
+  return (
+    <JumpHeartContainer>
+      <Canvas camera={{ fov: 120, position: [0, 0, 1], near: 0.1, far: 1000 }}>
         <Heart />
-    </Canvas>
-  </JumpHeartContainer>
+        <OrbitControls />
+      </Canvas>
+    </JumpHeartContainer>
+  );
 }
 
 export default JumpHeart;
