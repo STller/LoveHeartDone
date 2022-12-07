4558753e79fdce9cc6db9b220856133a2123315d
STller <1165975843@qq.com>
2022-12-07 20:45:59 +0800

    add audio

---
+++ src/pages/CornfieldChase/components/Track.tsx
@@ -0,0 +1,40 @@
+import { useFrame } from "@react-three/fiber";
+import { useEffect, useRef } from "react";
+import { suspend } from "suspend-react";
+import { InstancedMesh, Mesh, Object3D } from "three";
+import createAudio from "../utils/createAudio";
+
+interface propsType {
+    url: string;
+    y?: number;
+    space?: number;
+    width?: number;
+    height?: number;
+    obj?: Object3D;
+    [key: string]: any;
+}
+export default function Track (props: propsType) {
+    const ref = useRef<InstancedMesh>(null!)
+    const { url, y = 2500, space = 1.8, width = 0.01, height = 0.05, obj = new Object3D(), ...extraProps } = props
+    const { gain, context, update, data, } = suspend(() => createAudio(url), [url])
+    useEffect(() => {
+        gain.connect(context.destination)
+        return () => gain.disconnect()
+    }, [gain, context])
+    useFrame(() => {
+        let avg = update()
+        for (let i = 0; i < data.length; i++) {
+            obj.position.set(i * width * space - (data.length * width * space) / 2, data[i] / y, 0)
+            obj.updateMatrix();
+            (ref.current as any).setMatrixAt(i, obj.matrix)
+        }
+        (ref.current.material as any).color.setHSL(avg / 500, 0.75, 0.75)
+        ref.current.instanceMatrix.needsUpdate = true
+    })
+    return (
+        <instancedMesh castShadow ref={ref} args={[undefined, undefined, data.length]} {...props}>
+            <planeGeometry args={[width, height]} /> 
+            <meshBasicMaterial toneMapped={false} />
+        </instancedMesh>
+    )
+}
--- src/pages/CornfieldChase/index.tsx
+++ src/pages/CornfieldChase/index.tsx
@@ -1,23 +1,58 @@
 import { Canvas } from "@react-three/fiber";
-import { Suspense } from "react";
+import { Suspense, useState } from "react";
 import styled from "styled-components";
+import Track from "./components/Track";
+import './style.css'
+import Zoom from "./utils/zoom";
 
 const GalaxyContainer = styled.div`
   width: 100vw;
   height: 100vh;
   background-color: #000;
 `;
+const CornfieldChaseContainer = styled.div`
+    width: 100%;
+    height: 100%;
+    margin: 0;
+    padding: 0;
+    background: linear-gradient(15deg, rgb(82, 81, 88) 0%, rgb(255, 247, 248) 100%);
+    -webkit-touch-callout: none;
+    -webkit-user-select: none;
+    -khtml-user-select: none;
+    -moz-user-select: none;
+    -ms-user-select: none;
+    user-select: none;
+    overflow: hidden;
+    font-family: -apple-system, BlinkMacSystemFont, avenir next, avenir, helvetica neue, helvetica, ubuntu, roboto, noto, segoe ui, arial, sans-serif;
+`
 export default function CornfieldChase() {
+    const [ready, setReady] = useState(false)
   return (
-    <Canvas shadows dpr={[1, 2]} camera={{ position: [-1, 1.5, 2], fov: 25 }}>
-        <spotLight position={[-4, 4, -4]} angle={0.06} penumbra={1} castShadow shadow-mapSize={[2048, 2048]} />
-        <Suspense fallback={null}>
-
-        </Suspense>
-        <mesh receiveShadow rotation={[-Math.PI / 2, 0, 0]} position={[0, -0.025, 0]}>
-            <planeGeometry />
-            <shadowMaterial transparent opacity={0.15} />
-        </mesh>
-    </Canvas>
+    <CornfieldChaseContainer>
+        {
+            !ready && (
+                <div className={`fullscreen bg ${ready ? 'ready' : 'notready'} ${ready && 'clicked'}`}>
+                    <div className="stack">
+                        <button onClick={() => setReady(true)}>▶️</button>
+                    </div>
+                </div>
+            )
+        }
+        {
+            ready && (
+                <Canvas shadows dpr={[1, 2]} camera={{ position: [-1, 1.5, 2], fov: 25 }}>
+                    <spotLight position={[-4, 4, -4]} angle={0.06} penumbra={1} castShadow shadow-mapSize={[2048, 2048]} />
+                    <Suspense fallback={null}>
+                        <Track position-z={0} url={'/CornfieldChase.mp3'} />
+                        <Zoom url="/CornfieldChase.mp3" />
+                    </Suspense>
+                    <mesh receiveShadow rotation={[-Math.PI / 2, 0, 0]} position={[0, -0.025, 0]}>
+                        <planeGeometry />
+                        <shadowMaterial transparent opacity={0.15} />
+                    </mesh>
+                </Canvas>
+            )
+        }
+    </CornfieldChaseContainer>
   )
 }
